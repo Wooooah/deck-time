05059cb7353351dcfcc98111b91c2598bfe4a19a
Wooooah <148787786+Wooooah@users.noreply.github.com>
2026-08-09 19:42:26 -0400

    Create keyChallenges.js

---
+++ keyChallenges.js
@@ -0,0 +1,18 @@
+const keyChallenges = [
+    {
+        name: "Key Challenge 1",
+        description: "All players must answer the previous card."
+    },
+    {
+        name: "Key Challenge 2",
+        description: "Everyone must share their biggest lesson learned."
+    },
+    {
+        name: "Key Challenge 3",
+        description: "Choose another player to answer next."
+    },
+    {
+        name: "Key Challenge 4",
+        description: "Everyone votes on the best response."
+    }
+];
