c1e6accef56cfeb0971c7498cd094fa04a9e4e78
Wooooah <148787786+Wooooah@users.noreply.github.com>
2026-08-09 17:47:23 -0400

    Create cards.js

---
+++ cards.js
@@ -0,0 +1,262 @@
+const cards = [
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+
+    {
+        name: "Pump Failure",
+        description: "Describe the immediate operator response."
+    },
+
+    {
+        name: "Chemical Feed",
+        description: "How would you verify the feed rate?"
+    }
+];
