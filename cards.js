0f480da9ad67440a4e5e5978f93b2b438b029d8f
Wooooah <148787786+Wooooah@users.noreply.github.com>
2026-08-09 19:35:08 -0400

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,203 +1,193 @@
 const cards = [
 
     {
-        name: "Pump Failure",
+        name: "Took a Wrong Turn",
+        description: "Fall one floor, if on ground floor go back to entrance."
+    },
+
+    {
+        name: "Cobra Attack",
+        description: "Illness, You can only have maximum of 1 die until healed."
+    },
+
+    {
+        name: "Cobra Attack",
+        description: "Illness, You can only have maximum of 1 die until healed."
+    },
+
+    {
+        name: "Hapi's Flood",
+        description: "Players have 3 rounds to get out of the level before it floors and they lose all there dice, all actions tiles are disabled."
+    },
+
+    {
+        name: "Card to be Named #1",
+        description: "Roll your dice if you roll 6 or higher you live, if not lose 1 dice."
+    },
+
+    {
+        name: "Card to be Named #1",
+        description: "Roll your dice if you roll 6 or higher you live, if not lose 1 dice."
+    },
+
+    {
+        name: "Set's Storm",
+        description: "You are trapped in darkness, skip your roll for 2 turns"
+    },
+
+    {
+        name: "Cat got your tongue",
+        description: "Illness, You can not speak or make sounds for the rest of the game, until you are healed"
+    },
+
+    {
+        name: "Crocodiles",
+        description: "One the first floor place 1 crodile on the entrance square. Every round move the crocodile 1 square clockwise. If you touch a crocodile lose all your dice."
+    },
+
+    {
+        name: "Crocodiles",
+        description: "One the first floor place 1 crodile on the entrance square. Every round move the crocodile 1 square clockwise. If you touch a crocodile lose all your dice."
+    },
+
+    {
+        name: "Card to be named #3",
+        description: "You and a player roll a die if combined you roll 6 or below you both lose an item."
+    },
+
+    {
+        name: "Card to be named #3",
+        description: "You and a player roll a die if combined you roll 6 or below you both lose an item."
+    },
+
+    {
+        name: "Curse of Apep",
+        description: "Every player loses 1 die."
+    },
+
+    {
+        name: "Sekhmet's Bite",
+        description: "Loss a die."
+    },
+
+    {
+        name: "Sekhmet's Bite",
+        description: "Loss a die."
+    },
+
+    {
+        name: "Curse of Anubis",
+        description: "Illness, In 3 turns lose all your die"
+    },
+
+    {
+        name: "Granite Drum",
+        description: "This mechanical bobby trap returns you to the start of the floor"
+    },
+
+    {
+        name: "Granite Drum",
+        description: "This mechanical bobby trap returns you to the start of the floor"
+    },
+
+    {
+        name: "Pharoah's Curse",
         description: "Describe the immediate operator response."
     },
 
     {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
+        name: "Cave In",
+        description: "Go toward the entrance 5 spaces."
     },
 
     {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
+        name: "Card to be Named #2",
+        description: "You and another player roll a die the player who rolls lower loses a die."
     },
 
     {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
+        name: "Card to be Named #2",
+        description: "You and another player roll a die the player who rolls lower loses a die"
     },
 
     {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
+        name: "Card to be Named #4",
+        description: "You and another player move back 2 squares"
     },
 
     {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
+        name: "Card to be Named #5",
+        description: "Loss all your items"
     },
 
     {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
+        name: "Locus Attack",
+        description: "Roll a dice. On the floor matching the dice a locus attack will happen. Any player on that floor will lose 1 die"
     },
 
     {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
+        name: "Locus Attack",
+        description: "Roll a dice. On the floor matching the dice a locus attack will happen. Any player on that floor will lose 1 die"
     },
 
     {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
+        name: "Card to be Named #6",
+        description: "Loss an Item"
     },
 
     {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
+        name: "The Sun Disk Instant",
+        description: "Gain a die. After three rounds get burned and lose two die."
     },
 
     {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
+        name: "First Aid Kit Item",
+        description: "Heal any player 1 die or illness."
     },
 
     {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
+        name: "First Aid Kit Item",
+        description: "Heal any player 1 die or illness."
     },
 
     {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
+        name: "First Aid Kit Item",
+        description: "Heal any player 1 die or illness."
     },
 
     {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
+        name: "Torch Item",
+        description: "Roll 1 extra die this turn"
     },
 
     {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
+        name: "Torch Item",
+        description: "Roll 1 extra die this turn"
     },
 
     {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
+        name: "Bennu's Herron Item",
+        description: "Move to any other players square."
     },
 
     {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
+        name: "Magic Sandwich Instant",
+        description: "Returns every player to 2 die"
     },
 
     {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
+        name: "Magic Sandwich Instant",
+        description: "Returns every player to 2 die"
     },
 
     {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
+        name: "Shaky Ladder Item",
+        description: "Move anyone on your square up 1 Level."
     },
 
     {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
-    },
-
-    {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
-    },
-
-    {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
-    },
-
-    {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
-    },
-
-    {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
-    },
-
-    {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
-    },
-
-    {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
-    },
-
-    {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
-    },
-
-    {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
-    },
-
-    {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
-    },
-
-    {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
-    },
-
-    {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
-    },
-
-    {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
-    },
-
-    {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
-    },
-
-    {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
-    },
-
-    {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
-    },
-
-    {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
-    },
-
-    {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
-    },
-
-    {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
-    },
-
-    {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
-    },
-
-    {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
+        name: "Shaky Ladder Item",
+        description: "Move anyone on your square up 1 Level."
     },
 
     {
@@ -206,38 +196,48 @@ const cards = [
     },
 
     {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
+        name: "Scarab Ball Instant",
+        description: "Illness, Gain a die but you can only move counter clockwise from now on."
     },
 
     {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
+        name: "Slide Item",
+        description: "Move anyone on your square down one Levels."
     },
 
     {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
+        name: "Slide Item",
+        description: "Move anyone on your square down one Levels."
     },
 
     {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
+        name: "Eye of Horus Item",
+        description: "Gives the holder protection against any damage or illness. When the player would take damage dischard it."
     },
 
     {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
+        name: "Eye of Horus Item",
+        description: "Gives the holder protection against any damage or illness. When the player would take damage dischard it."
     },
 
     {
-        name: "Pump Failure",
-        description: "Describe the immediate operator response."
+        name: "Tyet Item",
+        description: "Skip a Action Card Effect"
     },
 
     {
-        name: "Chemical Feed",
-        description: "How would you verify the feed rate?"
+        name: "Tyet Item",
+        description: "Skip a Action Card Effect"
+    },
+
+    {
+        name: "Card to be named #Item",
+        description: "Give a player one of your die for a round."
+    },
+
+    {
+        name: "Smugglers bottle",
+        description: "Roll a die if you get 4 or higher gain 1 die."
     },
 
     {
