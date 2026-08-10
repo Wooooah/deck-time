ac6c0ca83ec5e511928558e152eefc29837ab2e4
Wooooah <148787786+Wooooah@users.noreply.github.com>
2026-08-09 20:06:42 -0400

    Update keyChallenges.js

--- keyChallenges.js
+++ keyChallenges.js
@@ -1,18 +1,25 @@
 const keyChallenges = [
     {
-        name: "Key Challenge 1",
-        description: "All players must answer the previous card."
+        name: "Bastet's Key",
+        description: "Place the cat 4 squares away from you, every turn before any player roles move the cat 2 squares toward to exit (stairs or entrance) if the cat makes it out of the entrance you lose this key. If you are on the same square before or after it moves you get the key."
     },
     {
-        name: "Key Challenge 2",
-        description: "Everyone must share their biggest lesson learned."
+        name: "Jump on the Jackel",
+        description: "Place a jackel 2 square away from you. Everyturn after the triggering player rolls move the jackel 3 tiles toward the triggerer, if the jackel moves onto the triggers square the player loses all their dice and the jackel disappers. To get the key a player must sneak up and jump the jackel from behind."
     },
     {
-        name: "Key Challenge 3",
-        description: "Choose another player to answer next."
+        name: "Key of Ra",
+        description: "The key appears by the entrance you have 6 rounds to get to it before it disapears."
     },
     {
-        name: "Key Challenge 4",
-        description: "Everyone votes on the best response."
+        name: "Shuyet Key",
+        description: "Roll a dice to determine which levels stair square the key starts on. After 3 rounds the roll the dice again. Move the key to that levels stair square. After the key has moved 2 times the key will disapear after 3 turns for good. Land on the same squareor move past the key square to get the key."
+    },
+        name: "Horus's Falcon",
+        description: "Roll a dice and place the falcon on the stair of the floor matching the number rolled. Land on the same square to catch the falcon and get the key. Every turn the falcon moves counter clockwise 1 corner. When moving if he contacts a player that player loses all their die. If the falcon contacts 2 players he will disappear."
+    },
+    {
+        name: "Key Challenge 6",
+        description: "Discard 6/8/10 items as a team to get the key. If you can't the key disappears."
     }
 ];
